67abdb9edc5b682de3732aba5ac02d0fdaf4fe3f
DivyChokshi <111312229+DivyChokshi@users.noreply.github.com>
2023-06-03 04:01:18 +0530

    new commit

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express=require('express');
 const app=express();
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/images', express.static(__dirname + "/images"));
 app.listen(3000,function(){
     console.log("the port is ready at 3000");
+})
+app.get("/",(req,res)=>{
+    console.log(req.body);
+    res.sendFile(__dirname + "/index.html");
+})
+app.post("/",(req,res)=>{
+    console.log(req.body);
+    res.send("hello");
+})
+app.post("/pokeball",(req,res)=>{
+    console.log(req.body);
+    res.sendFile(__dirname + "/pokeball/pokeball.html");
+})
+app.post('/NumberGuesser',(req,res)=>{
+    console.log(req.body);
+    res.send("hello");
+})
+app.post('/xyz',(req,res)=>{
+    console.log(req.body);
+    res.send("hellos");
 })
