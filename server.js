edbdd488e16f735c8bc5ff8693a6ac905272fd24
DivyChokshi <111312229+DivyChokshi@users.noreply.github.com>
2023-06-05 00:01:33 +0530

    addingscoreinthankspage

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/images', express.static(__dirname + "/images"));
 app.use(express.static(path.join(__dirname, 'pokeball')));
+app.engine('html', require('ejs').renderFile);
+// app.use(express.static(path.join(__dirname)));
 app.listen(3000,function(){
     console.log("the port is ready at 3000");
 })
@@ -20,6 +22,10 @@ app.post("/score",(req,res)=>{
     console.log(req.body);
     const val=Number(req.body.score);
     console.log(val);
+    const tosend='Congratulations for having a score of '+val +'!';
+    // const p1="Congratulations for having "+val+" score!"
+    // document.getElementById("scorevalue").innerText=p1;
+    res.render(__dirname + "/thanksspage.html", {name:tosend});
     res.sendFile(__dirname + "/thanksspage.html");
 })
 app.post("/",(req,res)=>{
